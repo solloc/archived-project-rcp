8697ffa1c2872f4ca5161abc1c6358983a18cbd3
solloc <matthias@maisch.me>
2017-10-06 19:31:41 +0200

    Reorg. superfluous services removed

--- com.novaimpact.project/src/com/novaimpact/project/service/ISecondTaskService.java
+++
@@ -1,7 +0,0 @@
-package com.novaimpact.project.service;
-
-import java.util.List;
-
-public interface ISecondTaskService {
-	public List<String> getTasks();
-}
--- com.novaimpact.project/src/com/novaimpact/project/service/ITaskService.java
+++
@@ -1,7 +0,0 @@
-package com.novaimpact.project.service;
-
-import java.util.List;
-
-public interface ITaskService {
-	public List<String> getTasks();
-}
