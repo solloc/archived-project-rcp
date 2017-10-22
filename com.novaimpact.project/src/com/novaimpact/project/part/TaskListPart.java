a99c50538a16427f5966ef9b675492bf9cf47ed2
solloc <matthias@maisch.me>
2017-10-22 18:27:00 +0200

    cleanup after WritableList experiments

--- com.novaimpact.project/src/com/novaimpact/project/part/TaskListPart.java
+++ com.novaimpact.project/src/com/novaimpact/project/part/TaskListPart.java
@@ -5,7 +5,6 @@ import javax.annotation.PostConstruct;
 
 import org.eclipse.core.databinding.beans.BeanProperties;
 import org.eclipse.core.databinding.observable.list.IObservableList;
-import org.eclipse.core.databinding.observable.list.WritableList;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.jface.databinding.viewers.ViewerSupport;
 import org.eclipse.jface.viewers.TableViewer;
@@ -20,7 +19,6 @@ import com.novaimpact.project.model.Task;
 import com.novaimpact.project.service.task.TaskService;
 
 public class TaskListPart {
-//	private LinkedList<Task> tableData;
 	private TableViewer tableViewer;
 
 	@PostConstruct
@@ -29,47 +27,23 @@ public class TaskListPart {
 			parent.setLayout(gl_parent);
 			
 			this.tableViewer = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
-//			this.tableViewer.setContentProvider(new ObservableListContentProvider());
-//			this.tableViewer.setContentProvider(new TaskTableContentProvider());
-//			this.tableViewer.setLabelProvider(new TaskTableLabelProvider());
-			
 			
 			Table table = this.tableViewer.getTable();
 			table.setLinesVisible(true);
 			table.setHeaderVisible(true);
-			table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+			table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 			
 			TableColumn tblclmnId = new TableColumn(table, SWT.NONE);
 			tblclmnId.setWidth(250);
 			tblclmnId.setText("ID");
-//			
+
 			TableColumn tblclmnName = new TableColumn(table, SWT.NONE);
 			tblclmnName.setWidth(100);
 			tblclmnName.setText("name");
-			
-			
-			
-			
-//			this.tableData = taskService.getTasks();
-//			IObservableList<Task> input = new WritableList<Task>(taskService.getTasks(), Task.class);
+
 			IObservableList<Task> input = taskService.getTasks();
 			
-//			IObservableList input = Properties.selfSet(Task.class).observe(taskService.getTasks());
-			
-//			ViewerSupport.bin
-			
-//			ViewerSupport.bind(this.tableViewer, input, BeanProperties.value(Task.class, "name"));
 			ViewerSupport.bind(this.tableViewer, input, BeanProperties.values(Task.class, new String[] {"id","name"}));
-			
-			
-//			input.add(new Task("abc"));
-//			input.add(new Task("def"));
-//			input.add(new Task("ghi"));
-//			input.add(new Task("jkl"));
-		
-			
-//			this.tableViewer.setInput(this.tableData);
-//			this.tableViewer.setInput(input);
 	}
 	
 	@Focus
--- com.novaimpact.project/src/com/novaimpact/project/service/task/TaskService.java
+++ com.novaimpact.project/src/com/novaimpact/project/service/task/TaskService.java
@@ -1,60 +1,35 @@
 package com.novaimpact.project.service.task;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
 import org.eclipse.core.databinding.observable.Realm;
-import org.eclipse.core.databinding.observable.list.IObservableList;
-import org.eclipse.core.databinding.observable.list.ObservableList;
 import org.eclipse.core.databinding.observable.list.WritableList;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Creatable;
-import org.eclipse.swt.widgets.Display;
 
 import com.novaimpact.project.model.Task;
 
 @Creatable
 public class TaskService {
 	
-//	private ObservableList<Task> tasks;
-	
-//	private LinkedList<Task> tasks = new LinkedList<Task>();
-	
-	private WritableList<Task> tasks; // = WritableList.withElementType(Task.class);
+	private WritableList<Task> tasks;
 	
 	@Inject
 	public TaskService(IEclipseContext context) {
 		context.set(TaskService.class.getName(), this);
 		
-//		LinkedList<Task> lTask = new LinkedList<Task>();
-//		lTask.add(new Task("123"));
-//		IObservableList<Task> lwTask = new WritableList<Task>(lTask,Task.class);
-		
 		Realm realm = Realm.getDefault();
 		if (realm == null) {
 			realm = new Realm() {
-				
 				@Override
 				public boolean isCurrent() {
-					// TODO Auto-generated method stub
 					return true;
 				}
 			};
 		}
 		
-//		ObservableList<Task> oTasks = new ObservableList<Task>(realm,this.tasks,Task.class) {};
-		
-//		Display.getDefault().get
-//		realm = Realm.getDefault();
-//		WritableList<Task> wList = new WritableList<Task>();
-		
-//		this.tasks = new LinkedList<Task>();
-//		this.tasks = new ObservableList<Task>();
 		this.tasks = new WritableList<Task>(realm);
 		
 		Task task1 = new Task(UUID.randomUUID().toString(), "task-1", "user-1", "descr-1");
@@ -71,10 +46,6 @@ public class TaskService {
 	public WritableList<Task> getTasks() {
 		return this.tasks;
 	}
-
-//	public LinkedList<Task> getTasks() {
-//		return this.tasks;
-//	}
 
 	public Task createTask() {
 		
